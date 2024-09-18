328036d635c3291749fdb1e3e288cb7076db346a
Gabriel <gabrielng.ys@gmail.com>
2024-09-18 23:46:26 +0800

    Added a function to add any product selected to the cart. swapped out the <img> tag to <svg> so the fill can be manipulated when hovered over.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+loadProducts();
+
 function loadProducts(){
     fetch('data.json')
         .then(response => response.json())
@@ -9,8 +11,8 @@ function loadProducts(){
                 const productDiv = document.createElement('div');
                 const productImage = document.createElement('img');
                 const productCategory = document.createElement('p');
-                const productTitle = document.createElement('h2');
-                const productPrice = document.createElement('h3');
+                const productTitle = document.createElement('p');
+                const productPrice = document.createElement('p');
                 const addCartButton = document.createElement('div');
 
                 // Add classes to elements
@@ -23,13 +25,16 @@ function loadProducts(){
                 productCategory.textContent = product.category;
                 productTitle.textContent = product.name;
                 productPrice.textContent = product.price;
+                addCartButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg><p>Add to Cart</p>';
 
                 //Append elements to productDiv
-                productDiv.append(productImage, productCategory, productTitle, productPrice, productPrice);
+                productDiv.append(productImage, productCategory, productTitle, productPrice, addCartButton);
                 productList.appendChild(productDiv);
             });
 
         })
 }
 
-loadProducts();
+function addToCart(){
+
+}

7dd00dc95b1315990e4b11e9653744a7929ebac2
Gabriel <gabrielng.ys@gmail.com>
2024-11-03 21:18:36 +0800

    Added containers to the product images so I could give the add to cart buttons a 'relative' positioned parent.
    Added styles to the button then clicked and when not clicked so the button text is spaced correctly.

--- script.js
+++ script.js
@@ -20,11 +20,13 @@ function loadProducts(){
                 const productTitle = document.createElement('p');
                 const productPrice = document.createElement('p');
                 const addCartButton = document.createElement('div');
+                const productImageDiv = document.createElement('div');
 
                 // Add classes to elements
                 productDiv.classList.add('product');
                 productImage.classList.add('product-img');
                 addCartButton.classList.add('add-cart-btn');
+                productImageDiv.classList.add('product-img-container')
 
                 // Add an ID to every product so it can be refered and added to the cart
                 productID = getCurrentId(index);
@@ -54,7 +56,8 @@ function loadProducts(){
                 });
 
                 //Append elements to productDiv and productList
-                productDiv.append(productImage, productCategory, productTitle, productPrice, addCartButton);
+                productImageDiv.append(productImage,addCartButton);
+                productDiv.append(productImageDiv, productCategory, productTitle, productPrice);
                 productList.appendChild(productDiv);
             });
 
@@ -93,6 +96,8 @@ function updateButton(i){
         addCartButton.append(addQuantityButton, productQuantity ,subtractQuantityButton);
         addCartButton.style.backgroundColor = 'var(--red)';
         addCartButton.style.color = 'var(--rose50)';
+        addCartButton.style.padding = '0.75em';
+        addCartButton.style.justifyContent = 'space-between'; 
         currentProductImage.style.border = "3px solid var(--red)"
 
         addQuantityButton.addEventListener('click',(event) => {
@@ -106,7 +111,9 @@ function updateButton(i){
     } else {
         addCartButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20"><g fill="#C73B0F" clip-path="url(#a)"><path d="M6.583 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM15.334 18.75a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3.446 1.752a.625.625 0 0 0-.613-.502h-2.5V2.5h1.988l2.4 11.998a.625.625 0 0 0 .612.502h11.25v-1.25H5.847l-.5-2.5h11.238a.625.625 0 0 0 .61-.49l1.417-6.385h-1.28L16.083 10H5.096l-1.65-8.248Z"/><path d="M11.584 3.75v-2.5h-1.25v2.5h-2.5V5h2.5v2.5h1.25V5h2.5V3.75h-2.5Z"/></g><defs><clipPath id="a"><path fill="#fff" d="M.333 0h20v20h-20z"/></clipPath></defs></svg><p>Add to Cart</p>';
         addCartButton.style.backgroundColor = 'var(--rose50)';
-        addCartButton.style.color = 'var(--black)';
+        addCartButton.style.color = 'var(--black)'
+        addCartButton.style.padding = '0.75em 2em';
+        addCartButton.style.justifyContent = 'center'; 
         currentProductImage.style.border = "none";
     }
 }
